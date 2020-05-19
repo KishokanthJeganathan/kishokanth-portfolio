0fd0524998365eae916d76af4af530c053184b95
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-05-19 15:39:02 +0200

    done

---
+++ src/components/global/blogPost/BlogPost.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import { Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styles from '../../global/blogPost/blogPost.module.css';
+import globalstyles from '../../global/global.module.css';
+import { IoIosArrowForward } from 'react-icons/Io';
+
+export default function BlogPost({ xs, sm, md, lg, nameOfProject, src, intro, slug }) {
+	return (
+		<Col xs={xs} sm={sm} md={md} lg={lg} className={styles.blogPost}>
+			<Img fluid={src} />
+			<div className={styles.content}>
+				<h3 className={globalstyles.h3}> {nameOfProject}</h3>
+				<p className={globalstyles.p2}>{intro}</p>
+				<Link to={`/blog/${slug}`} className={styles.readMore}>
+					Read Casestudy <IoIosArrowForward />
+				</Link>
+			</div>
+		</Col>
+	);
+}
--- src/components/home/blogsPosts/BlogPosts.js
+++ src/components/home/blogsPosts/BlogPosts.js
@@ -3,13 +3,33 @@ import { graphql, useStaticQuery } from 'gatsby';
 import { Row } from 'react-bootstrap';
 import Tittle from '../../global/Tittle/Tittle';
 import styles from '../blogsPosts/blogPosts.module.css';
+import { v4 as uuidv4 } from 'uuid';
+import BlogPost from '../../global/blogPost/BlogPost';
 
 const query = graphql`
 	{
+		allContentfulMyBlog(filter: { featureOnHomepage: { eq: true } }, sort: { fields: order, order: ASC }) {
+			nodes {
+				featureOnHomepage
+				order
+				slug
+				projectIntro {
+					internal {
+						content
+					}
+				}
+				headerimage {
+					fluid {
+						...GatsbyContentfulFluid
+					}
+				}
+				nameOfProject
+			}
+		}
 		allContentfulSectionTittles {
 			nodes {
-				tittle
 				subtittle
+				tittle
 			}
 		}
 	}
@@ -17,12 +37,26 @@ const query = graphql`
 
 export default function BlogPosts() {
 	const data = useStaticQuery(query);
-	const info = data.allContentfulSectionTittles.nodes[1];
-	const { subtittle, tittle } = info;
+	const SectionInfo = data.allContentfulSectionTittles.nodes[1];
+	const { subtittle, tittle } = SectionInfo;
+	const blogPosts = data.allContentfulMyBlog.nodes;
+	console.log(blogPosts);
 	return (
 		<section className={styles.blog}>
+			<Tittle tittle={`${tittle} ✍️`} subtittle={`${subtittle}`} />
 			<Row>
-				<Tittle tittle={`${tittle} ✍️`} subtittle={`${subtittle}`} />
+				{blogPosts.map((project) => (
+					<BlogPost
+						xs="12"
+						sm="6"
+						md="4"
+						key={uuidv4()}
+						nameOfProject={project.nameOfProject}
+						src={project.headerimage.fluid}
+						intro={project.projectIntro.internal.content}
+						slug={project.slug}
+					/>
+				))}
 			</Row>
 		</section>
 	);
