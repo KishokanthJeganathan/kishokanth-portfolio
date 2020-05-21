9346e8478311faa6debcf0aeaf6941510e1c54a4
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-05-21 12:46:46 +0200

    links working

--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -3,7 +3,7 @@ import Layout from '../components/global/layout/Layout';
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 import { Col, Row } from 'react-bootstrap';
 import styles from '../templates/blogTemplate.module.css';
-import { BLOCKS, MARKS } from '@contentful/rich-text-types';
+import { BLOCKS, MARKS, INLINES } from '@contentful/rich-text-types';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { tomorrowNightEighties } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
@@ -30,7 +30,10 @@ export const query = graphql`
 	}
 `;
 
+const website_url = 'http://localhost:8000/';
+
 const Bold = ({ children }) => <p className={styles.bold}>{children}</p>;
+
 const Code = ({ children }) => (
 	<SyntaxHighlighter style={tomorrowNightEighties} className={styles.code}>
 		{children}
@@ -49,6 +52,17 @@ export default function BlogTemplate({ data }) {
 		renderNode: {
 			'embedded-asset-block': (node) => {
 				return <img src={node.data.target.fields.file['en-US'].url} className={styles.img} />;
+			},
+			[INLINES.HYPERLINK]: (node) => {
+				return (
+					<a
+						href={node.data.uri}
+						target={`${node.data.uri.startsWith(website_url) ? '_self' : '_blank'}`}
+						rel={`${node.data.uri.startsWith(website_url) ? '' : 'noopener noreferrer'}`}
+					>
+						{node.content[0].value}
+					</a>
+				);
 			}
 		}
 	};
