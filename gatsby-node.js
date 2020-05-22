6be55b682a14b5172c8dc345de711d83b96f179e
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-05-22 16:35:42 +0200

    data visible

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,11 +1,27 @@
 const path = require(`path`);
 exports.createPages = async ({ graphql, actions }) => {
 	const { createPage } = actions;
-	const { data } = await graphql(`{ allContentfulMyBlog { edges { node { slug } } } }`);
+	const { data } = await graphql(`{ allContentfulMyBlog { edges { node { slug } } },  
+	allContentfulMyPortfolio {
+		edges {
+		  node {
+			slug
+		  }
+		}
+	  } }`);
+
 	data.allContentfulMyBlog.edges.forEach(({ node }) => {
 		createPage({
 			path: `blog/${node.slug}`,
 			component: path.resolve('./src/templates/blog-template.js'),
+			context: { slug: node.slug }
+		});
+	});
+
+	data.allContentfulMyPortfolio.edges.forEach(({ node }) => {
+		createPage({
+			path: `portfolio/${node.slug}`,
+			component: path.resolve('./src/templates/case-study.js'),
 			context: { slug: node.slug }
 		});
 	});
--- src/templates/case-study.js
+++ src/templates/case-study.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
-export default function caseStudy() {
+export const query = graphql`
+	query($slug: String) {
+		contentfulMyPortfolio(slug: { eq: $slug }) {
+			nameOfProject
+			projectIntro {
+				internal {
+					content
+				}
+			}
+		}
+	}
+`;
+
+export default function caseStudy({ data }) {
+	console.log(data);
 	return <div>I am a case study</div>;
 }
