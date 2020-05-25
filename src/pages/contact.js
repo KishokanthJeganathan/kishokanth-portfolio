45d526f38ec2228b9cbbe7cf011ccc5ec694498f
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-05-25 23:39:06 +0200

    ee
    dd

---
+++ src/pages/contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ReactTypeformEmbed } from 'react-typeform-embed';
+
+export default function contact() {
+	return (
+		<span>
+			<ReactTypeformEmbed url="https://kishokanthjeganathan.typeform.com/to/aWIhwZ" autoOpen />
+		</span>
+	);
+}
