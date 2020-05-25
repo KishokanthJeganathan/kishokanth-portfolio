cfcec1e6623e4963cebaa0de27075d8090a9e58c
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-05-25 12:11:40 +0200

    jbjkb

---
+++ src/components/error/Error.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Col } from 'react-bootstrap';
+import styles from '../error/error.module.css';
+import Links from '../../constants/Links';
+import { Link } from 'gatsby';
+import { v4 as uuidv4 } from 'uuid';
+
+export default function Error() {
+	return (
+		<Col className={styles.error}>
+			<h1>404</h1>
+			<p>Hmmmm, you appear to have hopped onto a page that I haven't created (yet 😉)</p>
+			Here are some links that might help you find what yu are looking for
+			<div className={styles.links}>
+				{Links.map((link) => (
+					<Link className={styles.link} to={link.path} key={uuidv4()}>
+						{link.text}
+					</Link>
+				))}
+			</div>
+		</Col>
+	);
+}
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import Layout from '../components/global/layout/Layout';
+import Error from '../components/error/Error';
+import SEO from '../components/global/seo';
 
 export default function errorPage() {
-	return <div>this is an error</div>;
+	return (
+		<Layout>
+			<SEO
+				title="404"
+				description="This page does not seem to exist. Try visiting the links you see on the device please"
+				image="https://i.ibb.co/GQ3G3Kp/404-page.jpg"
+			/>
+			<Error />
+		</Layout>
+	);
 }
