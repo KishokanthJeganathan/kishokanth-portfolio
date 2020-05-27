f281bd708b2ad2dd783265514eb12f688f4b26d2
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-05-27 11:25:04 +0200

    Update SocialSharing.js

--- src/components/global/socialSharing/SocialSharing.js
+++ src/components/global/socialSharing/SocialSharing.js
@@ -4,12 +4,13 @@ import { FaFacebookSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa';
 import styles from '../socialSharing/socialSharing.module.css';
 
 export default function SocialSharing() {
+	let url = typeof window !== 'undefined' && window.location.href;
 	return (
 		<Col>
 			<Row className={styles.shareButtonHolder}>
 				<Col xs={3} sm={2}>
 					<a
-						href={`https://facebook.com/sharer/sharer.php?u=${window.location.href}`}
+						href={`https://facebook.com/sharer/sharer.php?u=${url}`}
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label="Share on Facebook"
@@ -20,7 +21,7 @@ export default function SocialSharing() {
 				<Col xs={3} sm={2}>
 					<a
 						class="resp-sharing-button__link"
-						href={`https://twitter.com/intent/tweet/?text=${window.location.href}`}
+						href={`https://twitter.com/intent/tweet/?text=${url}`}
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label="share on Twitter"
@@ -31,7 +32,7 @@ export default function SocialSharing() {
 				<Col xs={3} sm={2}>
 					<a
 						class="resp-sharing-button__link"
-						href={` https://www.linkedin.com/sharing/share-offsite/?url=${window.location.href}`}
+						href={` https://www.linkedin.com/sharing/share-offsite/?url=${url}`}
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label="share on LinkedIn"
