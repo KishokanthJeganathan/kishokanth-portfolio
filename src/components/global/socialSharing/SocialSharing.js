cd4b07e2926e7246a6ca717204d9b811c25c38bb
KishokanthJeganathan <57392354+KishokanthJeganathan@users.noreply.github.com>
2020-06-01 15:53:20 +0200

    Update SocialSharing.js

--- src/components/global/socialSharing/SocialSharing.js
+++ src/components/global/socialSharing/SocialSharing.js
@@ -4,13 +4,13 @@ import { FaFacebookSquare, FaTwitterSquare, FaLinkedin } from 'react-icons/fa';
 import styles from '../socialSharing/socialSharing.module.css';
 
 export default function SocialSharing() {
-	let url = typeof window !== 'undefined' && window.location.href;
+	let url = window.location.pathname;
 	return (
 		<Col>
 			<Row className={styles.shareButtonHolder}>
 				<Col xs={3} sm={2}>
 					<a
-						href="https://www.facebook.com/sharer/sharer.php?u=https%3A//www.kishokanth.com/blog/how-to-easily-deploy-a-react-app"
+						href={`https://www.facebook.com/sharer/sharer.php?u=https%3A//www.kishokanth.com${url}`}
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label="Share on Facebook"
@@ -21,7 +21,7 @@ export default function SocialSharing() {
 				<Col xs={3} sm={2}>
 					<a
 						className="resp-sharing-button__link"
-						href="https://twitter.com/intent/tweet?text=https%3A//www.kishokanth.com/blog/how-to-easily-deploy-a-react-app"
+						href={`https://twitter.com/intent/tweet?text=https%3A//www.kishokanth.com${url}`}
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label="share on Twitter"
@@ -32,7 +32,7 @@ export default function SocialSharing() {
 				<Col xs={3} sm={2}>
 					<a
 						className="resp-sharing-button__link"
-						href="https://www.linkedin.com/shareArticle?mini=true&url=https%3A//www.kishokanth.com/blog/how-to-easily-deploy-a-react-app&title=&summary=&source="
+						href={`https://www.linkedin.com/shareArticle?mini=true&url=https%3A//www.kishokanth.com${url}&title=&summary=&source=`}
 						target="_blank"
 						rel="noopener noreferrer"
 						aria-label="share on LinkedIn"
